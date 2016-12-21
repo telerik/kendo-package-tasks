149092cd134ed4d9344fce897258de54915bba78
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-12-21 04:08:44 +0200

    fix: preserve ES modules in bundle

--- gulp-tasks.js
+++ gulp-tasks.js
@@ -1,13 +1,15 @@
-const path = require('path');
+const buble = require('gulp-buble');
+const buffer = require('vinyl-buffer');
+const commonTasks = require('@telerik/kendo-common-tasks');
 const jasmine = require('gulp-jasmine');
+const path = require('path');
+const rimraf = require('rimraf');
+const rollup = require('rollup-stream');
+const rollupBuble = require('rollup-plugin-buble');
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
 const specReporter = require('jasmine-spec-reporter');
 const webpackConfig = require('./webpack.config.js');
-const commonTasks = require('@telerik/kendo-common-tasks');
-const rollup = require('rollup-stream');
-const buble = require('rollup-plugin-buble');
-const sourcemaps = require('gulp-sourcemaps');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
 
 const SRC = "src/**/*.js";
 const TYPINGS = "src/**/*.d.ts";
@@ -21,21 +23,6 @@ module.exports = function(gulp, libraryName, options) {
     if (options && options.packageExternals) {
         webpackConfig.npmPackage.externals = webpackConfig.npmPackage.externals.concat(options.packageExternals);
     }
-
-    const rollupBundle = (format, dest) =>
-        rollup({
-            entry: 'src/main.js',
-            format: format,
-            sourceMap: true,
-            plugins: [ buble() ]
-        })
-        .pipe(source('main.js', 'src'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({
-            loadMaps: true
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(dest));
 
     commonTasks.addTasks(gulp, libraryName, SRC, webpackConfig, DTS);
 
@@ -59,13 +46,43 @@ module.exports = function(gulp, libraryName, options) {
     gulp.task('watch-e2e', (done) =>
         commonTasks.startKarma(done, e2eConfigPath, false));
 
-    gulp.task('rollup-es-bundle', [ 'bundle-typings' ], () => rollupBundle('es', 'dist/es'));
-    gulp.task('bundle-typings', () =>
+    gulp.task('clean-dist', (done) => rimraf('dist', done));
+
+    gulp.task('bundle-typings', [ 'clean-dist' ], () =>
         gulp.src(TYPINGS)
             .pipe(gulp.dest('dist/es'))
     );
 
-    gulp.task('rollup-cjs-bundle', () => rollupBundle('cjs', 'dist/npm'));
+    gulp.task('es-bundle', [ 'bundle-typings' ], () =>
+        gulp.src(SRC)
+            .pipe(sourcemaps.init())
+            .pipe(buble({
+                transforms: {
+                    modules: false
+                }
+            }))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('dist/es'))
+    );
 
-    gulp.task('build-rollup-package', [ 'rollup-cjs-bundle', 'rollup-es-bundle' ]);
+    gulp.task('cjs-bundle', [ 'clean-dist' ], () =>
+        rollup({
+            entry: 'src/main.js',
+            format: 'cjs',
+            sourceMap: true,
+            plugins: [ rollupBuble() ]
+        })
+        .pipe(source('main.js', 'src'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/npm'))
+    );
+
+    gulp.task('build-module', [ 'es-bundle', 'cjs-bundle' ]);
+
+    // Alias for backwards-compatibility
+    gulp.task('build-rollup-package', ['build-module']);
 };
